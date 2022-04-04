8cd75d5ea621388a46b320b24b9ae2f1634b7d71
Sabin Sunny <sabin.jb.24@gmail.com>
2022-04-04 13:08:38 +0530

    homepage ui changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlanComponent } from './plan/plan.component';
 import { HomeComponent } from './home/home.component';
-import { Convert24To12HourFormat } from './shared/Pipe24HrsTo12Transform';
+import { Convert24To12HourFormat } from './shared/Pipe24HrsTo12Transform.pipe';
+import { ConvertTimeToMealName } from './shared/PipeMealTimeToText.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     PlanComponent,
     HomeComponent,
-    Convert24To12HourFormat
+    Convert24To12HourFormat,
+    ConvertTimeToMealName
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
